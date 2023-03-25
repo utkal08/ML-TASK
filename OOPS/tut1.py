ac4c0a227949db1a6cf864561b84590fa85bde97
utkal08 <122446102+utkal08@users.noreply.github.com>
2023-03-25 22:29:50 +0530

    complete some more topics

--- OOPS/tut1.py
+++ OOPS/tut1.py
@@ -9,6 +9,7 @@
 #print(type(item1_price_total))#int 
 class Item:
   pay_rate =0.8 # The pay rate after 20% discount
+  all=[]
   def __init__(self,name:str,price:float,quantity):
       #Run validations to the recived argument
       assert price>=0, f"Price {price} is not greater than zero!"
@@ -20,6 +21,9 @@ class Item:
       self.quantity =quantity
       #print(f"An instance created: {name}")
       #def calculate_total_price(self,x,y):
+
+      # Actions to execute
+      Item.all.append(self)
   def calculate_total_price(self):
       return self.price * self.quantity
   def apply_discount(self):
@@ -27,13 +31,13 @@ class Item:
 
 
 #item1 =Item("phone",100 ,-5) this is done to explain assert 
-item1 =Item("phone",100 ,5)
+#item1 =Item("phone",100 ,5)
 #item1.name ="Phone"
 #item1.price =100
 #item1.quantity =5
 #print(item1.calculate_total_price(item1.price,item1.quantity))
 
-item2 =Item("Laptop",1000,3)
+#item2 =Item("Laptop",1000,3)
 #item2.name ="Laptop"
 #item2.price =1000
 #item2.quantity =3
@@ -56,10 +60,28 @@ item2 =Item("Laptop",1000,3)
 #print(item2.pay_rate)
 #print(Item.__dict__) # All the attributes for Class level 
 #print(item1.__dict__)# All the attributes for instance level
-item1.apply_discount()
-print(item1.price)
+#item1.apply_discount()
+#print(item1.price)
 
-item2=Item("laptop",1000,3)
-item2.pay_rate =0.7
-item2.apply_discount()
-print(item2.price)
+#item2=Item("laptop",1000,3)
+#item2.pay_rate =0.7
+#item2.apply_discount()
+#print(item2.price)
+
+def instantiate_from_csv(self):
+  def __repr__(self):
+   return f"Item('{self.name}',{self.price},{self.quantity})"
+   
+item1 =Item("phone",100,1)
+item1 =Item("laptop",1000,3)
+item1 =Item("Cable",10,5)
+item1 =Item("Mouse",50,5)
+item1 =Item("Keyboard",75,5)
+#print(Item.all)
+
+#for instance in Item.all:
+  # print(instance.name)
+
+
+  #### Class Vs Static Methods ######
+
